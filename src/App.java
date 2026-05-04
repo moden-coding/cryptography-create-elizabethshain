5c65134eb962859de5576126618fcf1bb06edc04
elizabethshain <29eshain@gannacademy.org>
2026-05-04 09:22:21 -0400

    done

--- src/App.java
+++ src/App.java
@@ -1,9 +1,37 @@
 public class App {
-    public static void main(String[] args)  {
-       
+    public static void main(String[] args) {
+        // encode("hello");
+        System.out.println(encode("hello"));
     }
 
-    public static String encode(String word){
-        return "";
+    public static String encode(String word) {
+String words = "";
+        int shift = 1;
+        for (int i = 0; i < word.length(); i++) {
+            // int shift = 1;
+            char letter = word.charAt(i);
+            int number = (int) letter;
+            number = number - 97;
+            number = shift + number;
+            number = number % 26;
+            number = number + 97;
+            char newletter = (char) (number);
+            words += newletter;
+            shift = shift * 2;
+            
+            // System.out.println(encode("hello"));
+        } 
+        
+    String words2 = "";
+        for (int i = words.length() - 1; i >= 0; i--) {
+            char letter = words.charAt(i);
+            words2 += letter;
+        
+
+
+            
+        }
+
+        return words2;
     }
 }
